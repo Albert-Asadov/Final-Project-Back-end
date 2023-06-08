7d1b6d1234f116d2e7cb3785270831ff3d2ef8d0
Albert-Asadov <albertma@code.edu.az>
2023-06-08 19:51:52 +0400

    problemlerin helli

---
+++ Razor Final Project Code Academy/Areas/RazorAdmin/Controllers/AccountAdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Razor_Final_Project_Code_Academy.Entities;
+
+namespace Razor_Final_Project_Code_Academy.Areas.RazorAdmin.Controllers
+{
+	public class AccountAdminController:Controller
+	{
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountAdminController(UserManager<User> userManager, SignInManager<User> signInManager)
+		{
+           _userManager = userManager;
+           _signInManager = signInManager;
+        }
+
+
+	}
+}
+
